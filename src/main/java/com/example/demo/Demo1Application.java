41246474892877b63abc8dd7f9418ef3b8defab3
mvatanasov18 <mvatanasov18@codingburgas.bg>
2023-01-03 15:29:10 +0200

    Change credentials

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -10,7 +10,9 @@ public class Demo1Application {
     public static void main(String[] args) {
 
         SpringApplication.run(Demo1Application.class, args);
-
+        System.out.println("http://localhost:8080/h2-console");
+        System.out.println("log with no user (user:<nothing> and password:<nothing> )");
+        System.out.println("JDBC URL= jdbc:h2:file:/data/demo");
     }
 
 }
