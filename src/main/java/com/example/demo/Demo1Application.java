e36c6b788f68f693377ec90fc77178a39972cfc0
mvatanasov18 <mvatanasov18@codingburgas.bg>
2023-01-10 15:41:48 +0200

    nz

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,11 +1,20 @@
 package com.example.demo;
 
 import java.sql.Date;
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo1Application {
+public class Demo1Application implements CommandLineRunner {
+    private  Logger logger= LoggerFactory.getLogger(this.getClass());
+@Autowired
+PlayerDao dao;
+
 
     public static void main(String[] args) {
 
@@ -15,4 +24,8 @@ public class Demo1Application {
         System.out.println("JDBC URL= jdbc:h2:file:/data/demo");
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("All Players Data: {}",dao.getAllPlayers());
+    }
 }
